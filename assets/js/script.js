8bb65c89fdb5f01e9479d983ae6852e4b04cbf13
spectocr <highstakes22@gmail.com>
2021-05-23 17:49:20 -0400

    putting the bones up

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//build them varies
+var apiKey = "1c631efa51faf50ecb47f29508debd1d";
+
+
+
+//build API Call
+/*
+var getWeatherForcast = function() {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?
+    lat= {lat}
+    &lon={lon}
+    &exclude={part}
+    &appid={API key}"
+};*/
