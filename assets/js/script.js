b516aee0ecb869a86fc8f530d93adb2b9eadc7be
spectocr <highstakes22@gmail.com>
2021-05-23 19:28:33 -0400

    adding logic and more structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,42 @@
+//queryselectors
+var userEnteredCity = document.querySelector("#userEnteredCity");
+var searchBtn = document.querySelector(".searchBtn");
+var cityNameDateEl = document.querySelector(".cityNameDate");
+
+
 //build them varies
 var apiKey = "1c631efa51faf50ecb47f29508debd1d";
+var apiUrl = ""
+
 
 
 
 //build API Call
-/*
 var getWeatherForcast = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?
-    lat= {lat}
-    &lon={lon}
-    &exclude={part}
-    &appid={API key}"
-};*/
+    var cityName = userEnteredCity.value.trim();
+    cityNameDateEl.textContent = cityName;
+    console.log(cityName);  
+     apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+     console.log(apiUrl)
+    
+     // fetch 
+    
+     fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+            console.log(response);
+        } else {
+            alert('Error: City not found');
+        }
+
+    }); 
+    
+};
+console.log(apiUrl)
+getWeatherForcast();
+
+searchBtn.addEventListener("click", getWeatherForcast);
+
+
